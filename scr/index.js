4547918f1616bad78007d154a2be55137e9e4182
sajaddadmandian <139868801+Sajaddadmandian@users.noreply.github.com>
2024-01-29 12:29:15 +0330

    import libraries and Define the port

--- scr/index.js
+++ scr/index.js
@@ -2,3 +2,10 @@ const express = require("express");
 const ejs = require("ejs");
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
+
+const app = express();
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server is Running on the Port${port}`);
+});
