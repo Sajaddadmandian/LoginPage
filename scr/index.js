d39abb068c8f3487d232412f4688c6aa5c7ef8e6
sajaddadmandian <139868801+Sajaddadmandian@users.noreply.github.com>
2024-01-29 12:33:40 +0330

    add get's method

--- scr/index.js
+++ scr/index.js
@@ -5,6 +5,13 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.render("login");
+});
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
 const port = 5000;
 app.listen(port, () => {
   console.log(`Server is Running on the Port${port}`);
